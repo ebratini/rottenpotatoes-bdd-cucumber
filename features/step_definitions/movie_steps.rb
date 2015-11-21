8690017c2220f6fd5c0782e27addfde4a59b1aa3
Edwin Bratini <edwin.bratini@gmail.com>
2015-11-21 16:22:49 +0000

    Happy paths for sorting movies by title and by release date

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  rgx = /#{e1}.*#{e2}/m
+  page.body.should =~ rgx
 end
 
 # Make it easier to express checking or unchecking several boxes at once
